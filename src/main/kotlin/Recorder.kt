7366ec1f556f65447440f178c9ec423d3c812f35
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-07-23 16:00:28 +0800

    播放录像功能支持跳过前面一定数量的协议

--- src/main/kotlin/Recorder.kt
+++ src/main/kotlin/Recorder.kt
@@ -20,6 +20,7 @@ import kotlin.concurrent.fixedRateTimer
 class Recorder {
     private var list: MutableList<recorder_line> = ArrayList()
     private var currentIndex = 0
+    private var skipCount = 0
 
     @Volatile
     var loading = false
@@ -70,7 +71,7 @@ class Recorder {
         }
     }
 
-    fun load(version: Int, recordId: String, player: HumanPlayer) {
+    fun load(version: Int, recordId: String, skipCount: Int, player: HumanPlayer) {
         val file = File("records/")
         if (!file.exists() || !file.isDirectory) {
             player.sendErrorMessage("录像不存在")
@@ -82,6 +83,7 @@ class Recorder {
             return
         }
         val recordFile = files[0]
+        this.skipCount = skipCount
         loading = true
         saveLoadPool.trySend {
             try {
@@ -111,6 +113,16 @@ class Recorder {
     }
 
     fun displayNext(player: HumanPlayer) {
+        if (skipCount > 0) {
+            player.send(reconnectToc { isEnd = false })
+            while (currentIndex <= skipCount && currentIndex < list.size) {
+                val line = list[currentIndex]
+                if ("wait_for_select_role_toc" != line.protoName && "select_role_toc" != line.protoName)
+                    player.send(line.protoName, line.messageBuf.toByteArray(), true)
+                currentIndex++
+            }
+            player.send(reconnectToc { isEnd = true })
+        }
         while (true) {
             if (!player.isActive) {
                 loading = false
