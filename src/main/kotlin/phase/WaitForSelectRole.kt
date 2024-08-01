9149ed28a8a0062aa9b2628ff957e9d4146fbe96
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-08-01 23:22:23 +0800

    移除开局座位显示(resolve #244)

--- src/main/kotlin/phase/WaitForSelectRole.kt
+++ src/main/kotlin/phase/WaitForSelectRole.kt
@@ -1,10 +1,13 @@
 package com.fengsheng.phase
 
 import com.fengsheng.*
-import com.fengsheng.protos.*
 import com.fengsheng.protos.Common.color.*
 import com.fengsheng.protos.Common.role.*
 import com.fengsheng.protos.Fengsheng.select_role_tos
+import com.fengsheng.protos.gameStartToc
+import com.fengsheng.protos.selectRoleToc
+import com.fengsheng.protos.selectRoleTos
+import com.fengsheng.protos.waitForSelectRoleToc
 import com.fengsheng.skill.RoleCache
 import com.fengsheng.skill.RoleSkillsData
 import com.google.protobuf.GeneratedMessage
@@ -94,7 +97,6 @@ data class WaitForSelectRole(val game: Game, val options: List<List<RoleSkillsDa
             roles.addAll(options[player.location].map { it.role }.ifEmpty { listOf(unknown) })
             waitingSecond = Config.WaitSecond * 2
             possibleSecretTask.addAll(game.possibleSecretTasks)
-            position = whoseTurn.getAlternativeLocation(player.location) + 1
         })
         if (game.players.size < 5)
             player.notifyIdentity()
