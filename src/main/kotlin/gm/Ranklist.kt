d317d4f3cff5be0d924c04fc2a1260481fdaafd4
奇葩の灵梦 <cutereimu@vip.qq.com>
2024-08-02 00:02:19 +0800

    支持查看赛季最高分排行

--- src/main/kotlin/gm/Ranklist.kt
+++ src/main/kotlin/gm/Ranklist.kt
@@ -6,7 +6,7 @@ import java.util.function.Function
 class Ranklist : Function<Map<String, String>, Any> {
     override fun apply(form: Map<String, String>): Any {
         val seasonRank = kotlin.runCatching { form["season_rank"]!!.toBoolean() }.getOrElse { false }
-        return if (seasonRank) Statistics.rankListImage.get()
+        return if (!seasonRank) Statistics.rankListImage.get()
         else Statistics.getSeasonRankList()
     }
 }
