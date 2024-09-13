e4cc678527cfd84e5cf776ca271d81379a1737c4
zc-meng <zc_meng@tju.edu.cn>
2024-09-13 20:36:50 +0800

    半藏的特殊优化

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -242,7 +242,8 @@ class Game(val id: Int, totalPlayerCount: Int, val actorRef: ActorRef) {
                 }
                 Statistics.addPlayerGameCount(playerGameResultList)
                 Statistics.calculateRankList()
-                QQPusher.push(this, declaredWinners, winners, addScoreMap, newScoreMap, humanPlayers.size > 1 || humanPlayers.playerName == "半藏")
+                QQPusher.push(this, declaredWinners, winners, addScoreMap, newScoreMap, humanPlayers.size > 1 ||
+                    humanPlayers[0].playerName == "半藏")
             }
             players.forEach { it!!.notifyWin(declaredWinners, winners, addScoreMap, newScoreMap) }
         }
